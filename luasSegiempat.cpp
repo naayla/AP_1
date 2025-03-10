cd2f77f767768149fdeab3cd84fc15179f83e73a
naayla <naylasyifa3006@gmail.com>
2025-03-10 09:14:04 +0700

    Pertemuan 2: Penggunaan Operator

---
+++ luasSegiempat.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float Luas P, L;
+
+    cout << "Masukkan panjang : ";
+    cin >> P;
+
+    cout << "Masukkan lebar : ";
+    cin >> L;
+
+    luas = P * L;
+    cout << "Luas = " << Luas << endl;
+
+    return 0;
+}
+
+
+
