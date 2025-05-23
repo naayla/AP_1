5bf6cb5b567da56fed78fdd9fdd2f53d0c11b34c
naayla <naylasyifa3006@gmail.com>
2025-05-23 19:58:30 +0700

    Pertemuan 4: Perulangan

--- Pertemuan 4/latihan2.cpp
+++ Pertemuan 4/latihan2.cpp
@@ -1,20 +1,22 @@
-#include <iostream>
+#include <iostream> // Header untuk c++
 using namespace std;
 
 int main() {
-    string kalimat;
-    int i;
+    string kalimat; // Variabel string untuk menyimpan kalimat
+    int i; // Deklarasi variabel integer untuk digunakan dalam perulangan
 
-    cout << "Masukkan kalimat : ";
-    getline(cin, kalimat);
+    cout << "Masukkan kalimat : "; // Inputan Pengguna
+    getline(cin, kalimat); // Membaca Inputan dan Menyimpnnya di variabel 'kalimat'
 
     // alya
-    for(i = 0; i < kalimat.length(); i++){
+    for(i = 0; i < kalimat.length(); i++){ // Perulangan untuk mengakses setiap karakter dalam string 'kalimat'
+        // Mengecek apakah karakter ke-i adalah huruf vokal kecil: a, i, u, e, o
         if (kalimat[i] == 'a' || kalimat[i] == 'i' || kalimat[i] == 'u' || kalimat[i] == 'u' || kalimat[i] == 'e' || kalimat[i] == 'o'){
-            kalimat[i] = toupper(kalimat[i]);
+            kalimat[i] = toupper(kalimat[i]); // Jika iya, ubah karakter tersebut menjadi huruf kapital
             }
     }
     cout << "Kalimat dalam huruf kapital : " << kalimat << endl;
+    // Menampilkan hasil akhir kalimat setelah huruf vokal kecil diubah menjadi kapital
 
     return 0;
 }
