5502910509b1e2eba514941197ac001f57a3b7e9
naayla <naylasyifa3006@gmail.com>
2025-05-24 18:09:15 +0700

    Pertemuan 8: Struct & Class

--- Pertemuan 8/aksesStruct.cpp
+++ Pertemuan 8/aksesStruct.cpp
@@ -1,16 +1,16 @@
-#include <iostream>
-#include <string>
+#include <iostream> // Header untuk c++
+#include <string> // Untuk tipe data string
 using namespace std;
 
 struct Mahasiswa {
-    string nama;
-    int umur;
-    float ipk;
+    string nama; n// String nama mahasiswa
+    int umur; // Umur Mahasiswa
+    float ipk; // Ipk Mahasiswa
 };
 
 int main() {
-    Mahasiswa mhs1;
-
+    Mahasiswa mhs1; // Membuat objek struct Mahasiswa bernama mhs1
+    // Data mahasiswa operator (.) karna variabel langsung
     mhs1.nama = "Nayla";
     mhs1.umur = 18;
     mhs1.ipk = 4.00;
@@ -20,8 +20,8 @@ int main() {
     // cout << "Umur : " << mhs1.umur << endl;
     // cout << "IPK : " << mhs1.ipk << endl;
 
-    Mahasiswa *ptrMhs = &mhs1;
-    cout << "Akses dengan -> : " << endl;
+    Mahasiswa *ptrMhs = &mhs1;  // Membuat pointer ke struct Mahasiswa dan menunjuk ke alamat mhs1
+    cout << "Akses dengan -> : " << endl; // Akses data mhs1 LEWAT POINTER pakai operator panah (->)
     cout << "Nama : " << ptrMhs->nama << endl;
     cout << "Umur : " << ptrMhs->umur << endl;
     cout << "IPK : " << ptrMhs->ipk << endl;
