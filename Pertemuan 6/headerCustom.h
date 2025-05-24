95ef4658f18c63660b1ff6151cbda968bcbd4dde
naayla <naylasyifa3006@gmail.com>
2025-05-24 17:44:18 +0700

    Pertemuan 6: Fungsi dan Header

--- Pertemuan 6/headerCustom.h
+++ Pertemuan 6/headerCustom.h
@@ -1,4 +1,4 @@
-#define  u using namespace std;
-#define L <<
-#define o cout
-#define e endl;
+#define  u using namespace std; //  Makro 'u' akan digantikan dengan 'using namespace std;'
+#define L << // Makro 'L' menggantikan operator `<<` pada cout.
+#define o cout // Makro 'o' digantikan dengan `cout`
+#define e endl; // Makro 'e' digantikan dengan `endl;`
