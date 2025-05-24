eada84dedb55476dd4877dc32f017ee056cc1724
naayla <naylasyifa3006@gmail.com>
2025-05-24 17:55:29 +0700

    Pertemuan 7: Pointer

--- Pertemuan 7/pointer.cpp
+++ Pertemuan 7/pointer.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
+#include <iostream> // Header untuk c++
 using namespace std;
 
-void penjumlahan(int a, int b) {
+void penjumlahan(int a, int b) { // Fungsi yang menerima nilai biasa
     cout << a + b << endl;
 }
 
-void penjumlahanPointer (int *a, int *b) {
+void penjumlahanPointer (int *a, int *b) { // Fungsi untuk pointer
     *a += *b;
     cout << *a << endl;
 }
@@ -19,14 +19,14 @@ int main() {
     system("cls");
 
     // Deklarasi Pointer
-    int number = 35;
-    int *pointer_number = &number;
+    int number = 35; // Variabel integer
+    int *pointer_number = &number; // Deklarasi pointer 'pointer_number' yang menunjuk ke alamat 'number'
 
     // cout << "Isi variabel number = " << number << endl;
-    // cout << "Alamat memori variabel number = " << &number << endl;
-    // cout << "Isi variabel pointer number = " << pointer_number << endl;
-    // cout << "Isi variabel yang ditunjuk oleh pointer_number = " << *pointer_number << endl;
-    // cout << "Alamat memori variabel pointer_number = " << &pointer_number << endl;
+    // cout << "Alamat memori variabel number = " << &number << endl; // Alamat 'number'
+    // cout << "Isi variabel pointer number = " << pointer_number << endl; // Alamat yang disimpan pointer
+    // cout << "Isi variabel yang ditunjuk oleh pointer_number = " << *pointer_number << endl; // Nilai di alamat yang ditunjuk
+    // cout << "Alamat memori variabel pointer_number = " << &pointer_number << endl; // Alamat pointer itu sendiri
     // cout << endl;
 
 
@@ -41,13 +41,13 @@ int main() {
 
 
     // Pointer in Array
-    int num[] = {1, 2, 3, 4, 5};
-    int *pointer_num = num;
+    int num[] = {1, 2, 3, 4, 5}; // Deklarasi array
+    int *pointer_num = num; // Pointer menunjuk ke elemen pertama array (num[0])
     // cout << "Isi variabel number = " << num[0] << endl;
-    // cout << "Alamat memori variabel num = " << &num[3] << endl;
-    // cout << "Isi variabel pointer_num = " << pointer_num << endl;
-    // cout << "Isi variabel yang ditunjuk oleh pointer_num = " << *pointer_num << endl;
-    // cout << "Alamat memori variabel pointer_num = " << &pointer_num << endl;
+    // cout << "Alamat memori variabel num = " << &num[3] << endl; // Alamat elemen ke-4 (indeks 3)
+    // cout << "Isi variabel pointer_num = " << pointer_num << endl; // Alamat num[0]
+    // cout << "Isi variabel yang ditunjuk oleh pointer_num = " << *pointer_num << endl; // Nilai num[0]
+    // cout << "Alamat memori variabel pointer_num = " << &pointer_num << endl; // Alamat pointer itu sendiri
     // cout << endl;
 
     // *pointer_num += 5; // 6
@@ -63,16 +63,16 @@ int main() {
     // int num1 = 5;
     // int num2 = 7;
     // penjumlahan(num1, num2);
-    // cout << num1 << endl;
-    // cout << num2 << endl;
+    // cout << num1 << endl; // Output: 5
+    // cout << num2 << endl; // Output: 7
     // penjumlahanPointer(&num1, &num2);
-    // cout << num1 << endl;
-    // cout << num2 << endl;
+    // cout << num1 << endl; // Output: 12 (karena num1 += num2 di fungsi)
+    // cout << num2 << endl; // Output: 7
 
     // Pointer in Pointer
     // int score = 4;
-    // int *pointer_score = &score;
-    // int **ptr_pointer_score = &pointer_score;
+    // int *pointer_score = &score;  // Pointer menunjuk ke 'score'
+    // int **ptr_pointer_score = &pointer_score; // Pointer in pointer
 
     // cout << "Isi variabel score " << score << " dan alamat memorinya " << &score << endl;
     // cout << "Isi variabel pointer_score " << pointer_score << " dan isi variabel yang ditunjuk oleh pointer score " << *pointer_score << " alamat memori pointer_score " << &pointer_score << endl;
@@ -80,12 +80,12 @@ int main() {
 
 
     // Dynamic Pointer
-    // int *ptr = new int;
-    // *ptr = 30;
-    // cout << "Isi variabel ptr = " << *ptr << " alamat variabel ptr = " << &ptr << endl;
+    // int *ptr = new int; // Mengalokasikan memori baru untuk satu integer di heap, 'ptr' menunjuk ke sana
+    // *ptr = 30; // Menetapkan nilai 30 ke lokasi memori yang ditunjuk oleh 'ptr'
+    // cout << "Isi variabel ptr = " << *ptr << " alamat variabel ptr = " << &ptr << endl; // Output nilai dan alamat pointer itu sendiri
 
     // delete ptr; // deallocation
-    // cout << "Isi variabel ptr = " << *ptr << " alamat variabel ptr = " << &ptr << endl;
+    // cout << "Isi variabel ptr = " << *ptr << " alamat variabel ptr = " << &ptr << endl; 
 
 
     // Contoh 
