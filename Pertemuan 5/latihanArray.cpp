c0e148c9b3d66c1754c5f0ae76ef22c5a545efd5
naayla <naylasyifa3006@gmail.com>
2025-05-24 17:25:19 +0700

    Pertemuan 5: Array & Vector

--- Pertemuan 5/latihanArray.cpp
+++ Pertemuan 5/latihanArray.cpp
@@ -1,33 +1,40 @@
-#include <iostream>
+#include <iostream> // Header untuk c++
 using namespace std;
 
 int main(){
 
     // penjumlahan matriks 2 x 2
-    int matriks1[2][2];
-    int matriks2[2][2];
-    int hasil[2][2];
-
+    int matriks1[2][2]; // Matriks pertama
+    int matriks2[2][2]; // Matriks kedua
+    int hasil[2][2]; // Matriks utnuk menyimpan hasil penjumlahan
+    
+    // Input elemen Matriks 1
     cout << "Matriks 1 : " << endl;
+    // Loop untuk baris
     for(int  i = 0; i < 2; i++) {
         for(int  j = 0; j < 2; j++) {
+            // Menginput elemen Matriks
             cout << "Masukkan elemen baris " << i + 1 << " kolom " << j + 1 << " : ";
             cin >> matriks1[i][j];
         }
 
     }
-
+    
+    // Input elemen Mateiks 2
     cout << "Matriks 1 : " << endl;
     for(int  i = 0; i < 2; i++) {
+        // Loop untuk kolom
         for(int  j = 0; j < 2; j++) {
             cout << "Masukkan elemen baris " << i + 1 << " kolom " << j + 1 << " : ";
             cin >> matriks2[i][j];
         }
 }
 
+    // Menghitung dan Menampikan hasi;
     cout << "Hasil Penjumlahan" << endl;
     for(int  i = 0; i < 2; i++) {
         for(int  j = 0; j < 2; j++) {
+            // Menjumlahkan elemen matriks dan menyimpannya
             hasil[i][j] = matriks1[i][j] + matriks2[i][j];
         cout << hasil[i][j] << " ";
     }
